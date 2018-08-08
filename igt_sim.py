bb4d27ecd4a8883ce40259a088c6e8bf4475cd9c
Hariprasad <hari.prasadx.r@gmail.com>
2018-08-08 15:09:14 +0530

    SSH login

--- igt_sim.py
+++ igt_sim.py
@@ -1,41 +1,51 @@
+#!/usr/bin/python3
 from pexpect import pxssh
-#import getpass
 import sys,os
-global CMD,ssh_client
-#CMD='/home/vpgba/Lohith/ww26/new/otc_gen_graphics-intel-gpu-tools/build/tests/'
-def LogIn(host,user,passwd):
-        ssh_client = pxssh.pxssh()
-        ssh_client.login(host,user,passwd,port=1340) #login credentials
-        s.sendline(cmd)	 # run a command
-        s.prompt()	 # match the prompt
-        print (s.before)	 # print everything before the prompt.
-        s.logout()
+ssh_client=''
+hostname= '10.223.161.28'       # host IP Address
+username = 'vpgba'              # User name
+passwd = 'vpgba'                # passsword
+port = 1340                     # port Address
+IGT_Path = 'sudo /home/vpgba/otc_gen_graphics-intel-gpu-tools_ww20/build/tests/'
+class LogSession():
+        def IGT_EXE_GRID(self):
+                print("IGT executed")
+                return IGT_Path+'testdisplay'
+        def IFT_EXE_GRID(self):
+                print("IFT executed")
+                return 'sudo cat /sys/kernel/debug/dri/0/i915_display_info'
+        def CMD(self):
+                print("CMD executed")
+                return 'ls'
+        def LOGIN(self,host,user,pswd,prt):
+                global ssh_client
+                ssh_client = pxssh.pxssh()
+                ssh_client.force_password = True
+                ssh_client.login(host,user,passwd,port = prt) #login credentials
+        def send_cmd(self,cmd):
+                ssh_client.sendline(cmd)         # run a command
+                ssh_client.prompt()      # match the prompt
+                print((ssh_client.before).decode('utf-8'))#print (ssh_client.after)        # print everything before the prompt.
+        def LOGOUT(self):
+                ssh_client.logout()
 
-try:
-	s = pxssh.pxssh()
-	hostname = '10.223.161.119'
-	username = 'vpgba'
-	LogIn(hostname,username,'vpgba')
-#    password = getpass.getpass('password: ')
-#        sel=sys.argv[1]
-#        test=sys.argv[2]
-#        if sel=='L':
-#                LogIn(hostname,username,'vpgba','ls /home/vpgba/Lohith')#(CMD+test+' --l'))
-#        elif sel=='F':
-#                LogIn(hostname,username,'vpgba',(CMD+test#))
-#	elif sel=='FS':
-#		fil=open("subtest.txt",'w')
-#		x=LogIn(hostname,username,'vpgba'(CMD+test+' --l '))
-#		fil.write(x)
-#		for i in fil.readlines():
-#			i=i.replace("\n","")
-#			LogIn((hostname,username,'vpgba'(CMD+test+' --r '+i)))
-#		fil.close()
- #       elif sel=='S':
- #               LogIn(hostname,username,'vpgba',(CMD+test+' --r '+sys.argv[3]));
-#
-except pxssh.ExceptionPxssh as e:
-        print("pxssh failed on login.")
-        print(e)
-finally:
-	print("Test Completed")
+if __name__ == "__main__":
+        log=LogSession()        # Declaration of LogSession class in main program
+        try:
+                value=sys.argv[1].upper()
+                print(str(hostname)+' *** '+username+' *** '+passwd+' *** '+str(port))
+                log.LOGIN(hostname,username,passwd,port)
+                if (value=='-CMD'):
+                        log.send_cmd(log.CMD())
+                elif (value=='-IGT'):
+                        log.send_cmd(log.IGT_EXE_GRID())
+                elif (value=='-IFT'):
+                       log.send_cmd(log.IFT_EXE_GRID())
+                else:
+                        raise ValueError('\n 1. python3 simple.py -cmd --> for manual command \n 2. python3 simple.py -igt --> for IGT execution \n 3. python3 simple.py -ift --> for IFT execution')
+                log.LOGOUT()
+        except pxssh.ExceptionPxssh as e:
+                print("pxssh failed on login.")
+                print(e)
+        finally:
+                print("Session Logged out")
